e52a67d9116e76354295bfda8b4421262571807a
Kendall Nakai <kendallnakai@gmail.com>
2021-02-13 21:59:29 -0800

    uh

--- components/product-item.js
+++ components/product-item.js
@@ -1,5 +1,6 @@
 // product-item.js
-
+/* 3. This will likely be the trickiest part. You will be creating the custom component <product-item> for the card. We’ve given you a small start in product-item.js, but make sure to read up on web components using the resources below. Thankfully, as far as the HTML goes we’ve already given you a sample <li> that you can just copy and modify. Make sure to change the img src, img alt, and the inner text of both p elements. Since you will also need the styles for the card since the page styles won’t work in the Shadow DOM, if you look in the styles.css file we’ve marked out the start and end of all the CSS you will need to include.
+*/
 class ProductItem extends HTMLElement {
   // TODO
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,19 @@
 // Script.js
 
+/********************************************************
+1. fetching data from an endpoint to get product items. 
+2. Once you have fetched this array, add it to local storage 
+*/
+
+// fires when webpage is ready
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+    // check before initial fetch request to see if it is in local storage
+    if(localStorage.getItem('itemArr') === null) {
+      // create a fetch request
+      fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      // .then(data => console.log(data));
+      // url return array of js obj
+      .then(data => localStorage.setItem('itemArr', JSON.stringify(data)));
+    }
 });
